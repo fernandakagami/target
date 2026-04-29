a1c6f62ce9aca4b8616241bceffa61ddc402602d
Fernanda <fernandakagami@gmail.com>
2026-04-29 16:41:40 -0300

    Create Loading component

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,7 @@
 import { colors } from '@/theme/colors'
 import { Stack } from 'expo-router'
 import { useFonts, Inter_400Regular, Inter_500Medium, Inter_700Bold } from '@expo-google-fonts/inter'
+import { Loading } from '@/components/Loading'
 
 export default function Layout() {
   const [fontsLoaded] = useFonts({
@@ -10,7 +11,7 @@ export default function Layout() {
   })
 
   if (!fontsLoaded) {
-    return null
+    return <Loading />
   }
 
   return (
