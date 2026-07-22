85f9b09ddda62b3a1913666e84d3258426afea45
Fernanda <fernandakagami@gmail.com>
2026-07-22 09:18:02 -0300

    Add gradient color

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,17 +1,10 @@
-import { View, Text, Button } from "react-native";
-import { router } from "expo-router";
-import { fontFamily } from "@/theme/fontFamily";
+import { HomeHeader } from "@/components/HomeHeader";
+import { View } from "react-native";
 
 export default function Index() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Text style={{ fontFamily: fontFamily.bold, fontSize: 34 }}>Expo Router</Text>
-
-      <Button title="Nova meta" onPress={() => router.navigate('/target')} />
-
-      <Button title="Transação" onPress={() => router.navigate('/transaction/123')} />
-
-      <Button title="Em Desenvolvimento" onPress={() => router.navigate('/in-progress/123')} />
+    <View style={{ flex: 1 }}>
+      <HomeHeader />
     </View>
   )
 }
