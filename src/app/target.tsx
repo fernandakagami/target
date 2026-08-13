fd4b28d72bb276cbb0b03712ffb9c0830a5c21ab
Fernanda <fernandakagami@gmail.com>
2026-08-13 14:56:22 -0300

    Add CurrencyInput

--- src/app/target.tsx
+++ src/app/target.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/Button'
+import { CurrencyInput } from '@/components/CurrencyInput'
 import { Input } from '@/components/Input'
 import { PageHeader } from '@/components/PageHeader'
 import { View } from 'react-native'
@@ -13,9 +14,12 @@ export default function Target() {
 
       <View style={{ marginTop: 32, gap: 24 }}>
         <Input
-          label="Nome da meta"
+          label="Nova meta"
           placeholder="Ex: Viagem para praia, Apple Watch"
         />
+
+        <CurrencyInput label="Valor alvo" value={24350.73} />
+
         <Button title="Salvar" />
       </View>
     </View>
